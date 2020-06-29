b0840db5534c6f62a13b73f28e054336639c300e
p <p@p-PC>
2020-06-30 02:00:26 +0530

    SECOND COMMIT

--- helloworld/src/testing/Sub_array.java
+++ helloworld/src/testing/Sub_array.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Sub_array {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-	int i,j,n,s,sum,start,end;
-	s=0;sum=0;start=0;end=0;
+	int i,j,n,s,sum,start,end,c;
+	s=0;sum=0;start=0;end=0;c=0;
 	int a[]={1,2,3,4,5};
 	n=a.length;
 	System.out.println("Please enter the sum of the original array");
 	sum=sc.nextInt();
+	if(sum==0)
+	{
+		System.out.println("INVALID");
+	}
 	for(i=0;i<n;i++)
 	{  s=0;
 		s=s+a[i];
@@ -16,18 +20,21 @@ public class Sub_array {
 	    	s=s+a[j];
 	    	if(sum-s==0){start=i;
 	    	end=j;
+	    	c++;
 	    	break;}	    	
      		
 	    	}
 	}
-  if(s==sum)    {  
+ if(c>0){
 	for(int k=start;k<=end;k++)
 {
 	System.out.print(a[k]+" ");
-}}
-   else{
-	     System.out.println("-1");
-     }
+}
+ }
+ else
+ {
+	 System.out.println(--c);
+ }
 
 	}
 

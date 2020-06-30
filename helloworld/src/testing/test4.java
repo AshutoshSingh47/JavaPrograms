4d83f14d8514bc134a67d8f30f1dcbb189cc790e
p <p@p-PC>
2020-06-30 10:48:14 +0530

    commit

---
+++ helloworld/src/testing/test4.java
@@ -0,0 +1,22 @@
+package testing;
+import java.util.Scanner;
+public class test4 {
+public static void sum()
+{
+	Scanner sc=new Scanner(System.in);
+	int a,b,s;
+	a=sc.nextInt();
+	b=sc.nextInt();
+	s=a+b;
+	System.out.println("sum : "+s);
+	
+}
+	public static void main(String[] args) {
+//	Scanner sc=new Scanner(System.in);
+//	int tc;
+//	tc=sc.nextInt();
+		sum();
+
+	}
+
+}

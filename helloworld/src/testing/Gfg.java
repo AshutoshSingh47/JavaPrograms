1d08109017e796ee4b42c03ff7dfc78fccf80774
p <p@p-PC>
2020-07-02 09:59:33 +0530

    Second commit

--- helloworld/src/testing/Gfg.java
+++ helloworld/src/testing/Gfg.java
@@ -1,23 +1,23 @@
 package testing;
 import java.util.Scanner;
 class Gfg {
-    public static void input(int size,int sum)
-    {
-        Scanner sc=new Scanner(System.in);
-         int a[]=new int[size];
-    for(int j=0;j<size;j++)
+public static void main(String args[]) {
+    Scanner sc=new Scanner(System.in);
+    int tc,size,sum,i,j,s,start,end,c;
+     s=0;start=0;end=0;c=0;
+    System.out.println("enter the test cases");
+    tc=sc.nextInt();
+        for(int k=1;k<=tc;k++){
+        size=sc.nextInt();
+         sum=sc.nextInt();
+        int a[]=new int[size];
+    for(j=0;j<size;j++)
     {
         a[j]=sc.nextInt();
     }
-    sub_array(a,size,sum);
-    }
-    public static void sub_array(int a[],int size,int sum)
-    {
-       int i,j,s,start,end,c;
-       s=0;start=0;end=0;c=0;
        for(i=0;i<size;i++)
        {s=0;
-       s=s+a[i];
+         s=s+a[i];
            for(j=i+1;j<size;j++)
            {
                s=s+a[j];
@@ -31,24 +31,7 @@ class Gfg {
            }
        }
        if(c>0){
-       output(start,end);}
-       else{System.out.println(--c);}
-    }
-    public static void output(int start,int end)
-    {
-        System.out.print(start+" "+end);
-    }
-
-public static void main(String args[]) {
-    Scanner sc=new Scanner(System.in);
-    int tc,size,sum,i;
-    System.out.println("enter the test cases");
-    tc=sc.nextInt();
-    for(i=1;i<=tc;i++)
-    {
-        size=sc.nextInt();
-        sum=sc.nextInt();
-        input(size,sum);
+    	   System.out.println(start+" "+end); }
+       else{System.out.println(--c);}}
     }
   }
-}
--- helloworld/src/testing/test4.java
+++ helloworld/src/testing/test4.java
@@ -1,30 +1,28 @@
 package testing;
 import java.util.Scanner;
 public class test4 {
-public static void sum(int sp)
+public static void sum(int tc)
 {
 	Scanner sc=new Scanner(System.in);
-	int a,b,s;
+	int a,b;
+	int s[]=new int[tc];
+	for(int i=0;i<tc;i++){
 	a=sc.nextInt();
 	b=sc.nextInt();
-	s=a+b;
-	s=s*sp;
-	System.out.println("sum : "+s);
-	
+	s[i]=a+b;
+	}
+	for(int i=0;i<tc;i++){
+	System.out.println(s[i]+" ");}
 }
 	public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
-	int tc,sp;
+	int tc;
 	System.out.println("console working");
 	tc=sc.nextInt();
-	System.out.println("enter the no.");
-	sp=sc.nextInt();
 	for(int i=1;i<=tc;i++)
 	{
-		
-		sum(sp);
+		sum(tc);
 	}
 
 	}
-
 }

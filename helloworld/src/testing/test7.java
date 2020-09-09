cd3d60825b6e5a0d2733775fca73c7323ea5c532
Ashutosh-Tester <muskelon1111@gmail.com>
2020-09-09 11:17:52 +0530

    Batch : AshutoshSingh47 at 11:17 A.M.

---
+++ helloworld/src/testing/test7.java
@@ -0,0 +1,15 @@
+package testing;
+
+public class test7 {
+
+	public static void main(String[] args) {
+	int a[]=new int[10];
+	int n=a.length;
+//	for(int i=0;i<n;i++)
+//	{
+		System.out.print(a[0]+" ");
+//	}
+
+	}
+
+}

a9df01e83a64a2e2f652b0d511978d874fa493fd
Ashutosh-Tester <muskelon1111@gmail.com>
2020-09-16 12:11:14 +0530

    Batch : AshutoshSingh47 at 12:11 P.M.

---
+++ helloworld/src/testing/Array_3D.java
@@ -0,0 +1,34 @@
+package testing;
+import java.util.Scanner;
+public class Array_3D {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	int i,j,k;
+	int a[][][]={{{1,2},{3,4},{5,6}},{{7,8},{9,10},{11,12}}};
+//for(i=0;i<2;i++)
+//{
+//	for(j=0;j<3;j++)
+//	{
+//		for(k=0;k<2;k++)
+//		{
+//			a[k][j][i]=sc.nextInt();
+//		}
+//	}
+//}
+for(i=0;i<2;i++)
+{
+	for(j=0;j<3;j++)
+	{
+		for(k=0;k<2;k++)
+		{
+			System.out.print(a[k][j][i]+" ");
+		}
+		System.out.println();
+
+	}
+	System.out.println();
+}
+	}
+
+}
--- helloworld/src/testing/test9.java
+++ helloworld/src/testing/test9.java
@@ -3,8 +3,19 @@ package testing;
 public class test9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i,j;
+		i=0;j=0;
+		int a[]={1,2,3,4,5};
+		int b[]={2,5,6,7};
+		while(i<a.length && j<b.length)
+		{  
+			if(a[i]<b[j])
+			System.out.print(a[i++]+" ");
+			else if(a[i]>b[j])
+			System.out.print(b[j++]+" ");
+			else //if(a[i]==b[j])
+				i++;
+			System.out.print(b[j++]+" ");
+		}
 	}
-
 }
